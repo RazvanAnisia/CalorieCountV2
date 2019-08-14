baaecbe7067eb2fc2597e66653f8ef37c353e551
Razvan Anisia <anisia.razvan@gmail.com>
2019-08-14 19:11:37 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -22,85 +22,96 @@ PHASE:3
 -Create Rest architecture for the backend + CRUD, for adding new items, editing, etc
 */
 
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Quagga from "quagga"; // ES6
 
-import React , {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Quagga from 'quagga'; // ES6
-
-class App extends Component{
-  state ={
-    productName:'',
-    productPhoto:'',
-    productCategories:'',
-    productKeywords:'',
-    hideCamera:false,
-    addedProducts:[]
-
-  }
-  componentDidMount = () => {
-
-  }
-    initiateBarcodeDetection = () => {
-      if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-        // safely access `navigator.mediaDevices.getUserMedia`
+class App extends Component {
+  state = {
+    productName: "",
+    productPhoto: "",
+    productCategories: "",
+    productKeywords: "",
+    hideCamera: false,
+    addedProducts: []
+  };
+  componentDidMount = () => {};
+  initiateBarcodeDetection = () => {
+    if (
+      navigator.mediaDevices &&
+      typeof navigator.mediaDevices.getUserMedia === "function"
+    ) {
+      // safely access `navigator.mediaDevices.getUserMedia`
       this.setState({
-        productName:'',
-        productPhoto:'',
-        productCategories:'',
-        productKeywords:'',
-        hideCamera:false,
-
-      })
+        productName: "",
+        productPhoto: "",
+        productCategories: "",
+        productKeywords: "",
+        hideCamera: false
+      });
       var that = this;
-      Quagga.init({
-        inputStream : {
-          name : "Live",
-          type : "LiveStream",
-          target: document.querySelector('.camera-container')
-              // Or '#yourElement' (optional)
+      Quagga.init(
+        {
+          inputStream: {
+            name: "Live",
+            type: "LiveStream",
+            target: document.querySelector(".camera-container")
+            // Or '#yourElement' (optional)
+          },
+          numOfWorkers: 1,
+          decoder: {
+            readers: [
+              "ean_reader",
+              "code_128_reader",
+              "ean_8_reader",
+              "code_39_reader",
+              "code_39_vin_reader",
+              "codabar_reader",
+              "upc_reader",
+              "upc_e_reader",
+              "i2of5_reader"
+            ]
+          },
+          locate: true,
+          debug: {
+            drawBoundingBox: true,
+            showFrequency: true,
+            drawScanline: true,
+            showPattern: true
+          }
         },
-        numOfWorkers: 1,
-        decoder : {
-          readers: ['ean_reader', 'code_128_reader', 'ean_8_reader', 'code_39_reader', 'code_39_vin_reader',
-           'codabar_reader', 'upc_reader', 'upc_e_reader', 'i2of5_reader'],
-        },
-        locate:true,
-        debug: {
-          drawBoundingBox: true,
-          showFrequency: true,
-          drawScanline: true,
-          showPattern: true
-      }
-      }, function(err) {
+        function(err) {
           if (err) {
-              console.log(err);
-              return
+            console.log(err);
+            return;
           }
           console.log("Initialization finished. Ready to start");
-          Quagga.onDetected(function callback(data){
-            data.codeResult ? console.log(data.codeResult.code) : console.log('nope')
-            const productCode = data.codeResult.code
-          if (productCode) {
-            fetch(`https://world.openfoodfacts.org/api/v0/product/${productCode}`)
-            .then(res => res.json())
-            .then(data => {
-              if(data.status_verbose === 'product found'){
-                console.log(data.product)
-                that.setState({
-                  productName:data.product.product_name,
-                  productPhoto: data.product.image_front_url,
-                  productCategories: data.product.categories,
-                  productKeywords:data.product._keywords,
-                  hideCamera:true
-                })
-                Quagga.stop();
-              }
-
-            })
-          }
-
-          })
+          Quagga.onDetected(function callback(data) {
+            data.codeResult
+              ? console.log(data.codeResult.code)
+              : console.log("nope");
+            const productCode = data.codeResult.code;
+            if (productCode) {
+              fetch(
+                `https://world.openfoodfacts.org/api/v0/product/${productCode}`
+              )
+                .then(res => res.json())
+                .then(data => {
+                  if (data.status_verbose === "product found") {
+                    console.log(data.product);
+                    that.setState({
+                      productName: data.product.product_name,
+                      productPhoto: data.product.image_front_url,
+                      productCategories: data.product.categories,
+                      productKeywords: data.product._keywords,
+                      hideCamera: true
+                    });
+                    Quagga.stop();
+                  }
+                });
+            }
+          });
           /*
           What I could do here, is take the result, wait for 1-2 seconds, and keep checking if the result is the same,if not then update the value
            and use it to fetch frokm the food facts api
@@ -113,79 +124,71 @@ class App extends Component{
           //   console.log(data)
           // })
           Quagga.start();
-      });
-
-
-   }
-
+        }
+      );
     }
-    stopBarcodeDetection = () => {
-      Quagga.stop();
-      this.setState({
-        productCategories:'',
-        productKeywords:'',
-        productName:'',
-        productPhoto:'',
-      })
-    }
-    addProduct = () => {
-      // console.log(this.state.addedProduct.length)
-      // if (this.state.addedProducts.length !== 0) {
-      //   const addedProducts = this.state.addedProducts.push(this.state.productName);
-      //   console.log(addedProducts)
-      // this.setState({
-      //   addedProducts:addedProducts
-      // })
-      // }
+  };
+  stopBarcodeDetection = () => {
+    Quagga.stop();
+    this.setState({
+      productCategories: "",
+      productKeywords: "",
+      productName: "",
+      productPhoto: ""
+    });
+  };
+  addProduct = () => {
+    // console.log(this.state.addedProduct.length)
+    // if (this.state.addedProducts.length !== 0) {
+    //   const addedProducts = this.state.addedProducts.push(this.state.productName);
+    //   console.log(addedProducts)
+    // this.setState({
+    //   addedProducts:addedProducts
+    // })
+    // }
+  };
 
-    }
-
-  render (){
-    console.log(this.state)
-      return (
+  render() {
+    console.log(this.state);
+    return (
       <div className="App">
         <p>Find your food products</p>
-        <button onClick={this.initiateBarcodeDetection}>
-            Start
-        </button>
-        <button onClick={this.stopBarcodeDetection}>
-            Stop
-        </button>
-        <div className='camera-container'
-         style={this.state.hideCamera ? {display:'none'} : null}>
-        </div>
+        <button onClick={this.initiateBarcodeDetection}>Start</button>
+        <button onClick={this.stopBarcodeDetection}>Stop</button>
+        <div
+          className="camera-container"
+          style={this.state.hideCamera ? { display: "none" } : null}
+        />
 
-        <div className='product-container'>
-
-         {this.state.productName
-          ? (<div>
-              <p>Product name:
-               <strong>{this.state.productName}
-               </strong>
+        <div className="product-container">
+          {this.state.productName ? (
+            <div>
+              <p>
+                Product name:
+                <strong>{this.state.productName}</strong>
               </p>
-             <button onClick={this.addProduct}>Add Product</button>
-            </div>)
-          : null}
-         { this.state.productCategories
-          ? <p>Category:{this.state.productCategories}</p>
-          : null
-         }
-         { this.state.productKeywords
-          ? this.state.productKeywords.map((keyword, index) =>
-          (<li key='index'>{keyword}</li>))
-          : null
-         }
-          {this.state.productPhoto
-           ? <img src={this.state.productPhoto} />
-           : null }
+              <button onClick={this.addProduct}>Add Product</button>
+            </div>
+          ) : null}
+          {this.state.productCategories ? (
+            <p>Category:{this.state.productCategories}</p>
+          ) : null}
+          {this.state.productKeywords
+            ? this.state.productKeywords.map((keyword, index) => (
+                <li key="index">{keyword}</li>
+              ))
+            : null}
+          {this.state.productPhoto ? (
+            <img src={this.state.productPhoto} />
+          ) : null}
         </div>
-        <div className='summary'>
+        <div className="summary">
           {console.log(this.state.addedProducts)}
-            {this.state.addedProducts
-            ? this.state.addedProducts.map((prod,index) =>(
-              <li key={index}>{prod}</li>
-            ))
-          :null}
+          {this.state.addedProducts
+            ? this.state.addedProducts.map((prod, index) => (
+                <li key={index}>{prod}</li>
+              ))
+            : null}
         </div>
       </div>
     );
